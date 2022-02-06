eacf80daf7c95b37bf9817420c31f6b9e8ab3085
Jos van Nijnatten <9002122+vanNijnatten@users.noreply.github.com>
2022-02-06 22:15:34 +0100

    Edit the Readme file

--- update-modules.sh
+++ update-modules.sh
@@ -1,1 +1,9 @@
 git submodule update --init --recursive
+
+for dir in $(find modules -maxdepth 1 -mindepth 1 -type d)                                                                                        ✔  11:48:16 
+do
+ pushd "$dir"
+ git pull origin main
+ popd
+done
+
